14eb7bb873c25c1094fcfec74fa318d3e59d4714
David <romangaming0809@gmail.com>
2025-04-15 11:39:15 +0300

    Medium update
    added cards and arranged them

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -21,4 +21,33 @@ $this->title = 'Skill Swap';
             </div>
         </div>
     </div>
+    <div class="index-description">
+        <p class="index-description-text"><?= Yii::t('app', 'Everday is an oportunity to learn! Here you can learn anything from anyone, anyday. Come join us now!') ?></p>
+    </div>
+    <div class="card-flex">
+        <div class="card" style="width: 17rem;">
+            <h5 class="card-header"><?= Yii::t('app', 'Course 1') ?></h5>
+            <div class="card-body">
+                <p class="card-text"><?= Yii::t('app', 'Some quick example text to build on the card title and make up the bulk of the cards content') ?>.</p>
+                <a href="#" class="btn btn-primary"><?= Yii::t('app', 'Go somewhere') ?></a>
+            </div>
+        </div>
+        <div class="card" style="width: 17rem;">
+            <h5 class="card-header"><?= Yii::t('app', 'Course 2') ?></h5>
+            <div class="card-body">
+                <p class="card-text"><?= Yii::t('app', 'Some quick example text to build on the card title and make up the bulk of the cards content') ?>.</p>
+                <a href="#" class="btn btn-primary"><?= Yii::t('app', 'Go somewhere') ?></a>
+            </div>
+        </div>
+        <div class="card" style="width: 17rem;">
+            <h5 class="card-header"><?= Yii::t('app', 'Course 3') ?></h5>
+            <div class="card-body">
+                <p class="card-text"><?= Yii::t('app', 'Some quick example text to build on the card title and make up the bulk of the cards content') ?>.</p>
+                <a href="#" class="btn btn-primary"><?= Yii::t('app', 'Go somewhere') ?></a>
+            </div>
+        </div>
+    </div>
+    <div class="trend-art">
+        <h2><?= Yii::t('app', 'Trending articles') ?></h2>
+    </div>
 </div>
