981cff676c65e68159b7711913a24cd700e6b2ec
Huțanu Andrei <78648231+AndreicuD@users.noreply.github.com>
2025-04-29 11:10:46 +0300

    Ratings Start

---
+++ common/models/Rating.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\data\ActiveDataProvider;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\web\IdentityInterface;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+
+/**
+ * Rating model
+ *
+ * @property integer $id [int(auto increment)]
+ * @property integer $user_id [int(11)]
+ * @property integer $article_id [int(11)]
+ * @property integer $value [int(11)]
+ * 
+ * @property integer $created_at [datetime]
+ * @property integer $updated_at [timestamp = current_timestamp()]
+ *
+ *
+ */
+class Rating extends ActiveRecord
+{
+    const STATUS_PRIVATE = 0;
+    const STATUS_PUBLIC = 1;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName(): string
+    {
+        return '{{%transaction}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules(): array
+    {
+        return [
+            [['user_id', 'article_id'], 'required', 'on' => 'default'],
+            [['user_id', 'article_id', 'value'], 'required', 'on' => 'create'],
+
+            [['user_id', 'article_id', 'value'], 'safe'],
+            [['user_id', 'article_id', 'value'], 'safe', 'on' => 'search'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'article_id' => Yii::t('app', 'Article ID'),
+            'value' => Yii::t('app', 'Value'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors(): array
+    {
+        return [
+            'timestamp' => [
+                'class' => TimestampBehavior::class,
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+
+    /**
+     * Returns the object (with the same id) if found.
+     */
+    public static function findRating($user_id, $article_id): Transaction|IdentityInterface|null
+    {
+        return static::findOne(['user_id' => $user_id, 'article_id' => $article_id]);
+    }
+    /**
+     * Returns the profit for an article found by id.
+     */
+    public static function calculateRating($article_id): int
+    {
+        $rating_median = 0;
+        $ratings = static::findAll(['article_id' => $article_id]);
+        foreach($ratings as $rating) {
+            $rating_median += $rating->value;
+        }
+	$rating_median /= count($ratings);
+        return $rating_median;
+    }
+    
+    /**
+     * Creates data provider instance with search query applied
+     * used to create lists / grids
+     *
+     * @param array $params
+     * @return ActiveDataProvider
+     */
+    public function search(array $params): ActiveDataProvider
+    {
+        $this->scenario = 'search';
+
+        $query = self::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort'=> [
+                'defaultOrder' => ['id'=>SORT_DESC],
+            ]
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'user_id', $this->user_id])
+            ->andFilterWhere(['like', 'article_id', $this->article_id])
+            ->andFilterWhere(['like', 'value', $this->value])
+            ->andFilterWhere(['like', 'created_at', $this->created_at])
+            ->andFilterWhere(['like', 'updated_at', $this->updated_at]);
+
+        return $dataProvider;
+    }
+
+    /**
+     * Finds ratings by user id.
+     *
+     * @param string $id
+     * @return array|null
+     */
+    public static function findByUserId($id): null|array
+    {
+        return static::findAll(['user_id' => $id]);
+    }
+
+    /**
+     * Finds ratings by article id.
+     *
+     * @param string $id
+     * @return array|null
+     */
+    public static function findByArticleId($id): null|array
+    {
+        return static::findAll(['article_id' => $id]);
+    }
+}
