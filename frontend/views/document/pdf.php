e263ebdba6e6a2a4d573a96e1873fde1543ac565
AndreicuD <78648231+AndreicuD@users.noreply.github.com>
2025-07-26 13:45:41 +0300

    Certificate Modifications

--- frontend/views/document/pdf.php
+++ frontend/views/document/pdf.php
@@ -1,3 +1,4 @@
-<body>
-    <h1><?= $title ?></h1>
+<body style='background: linear-gradient(138deg,rgba(204, 255, 241, 1) 0%, rgba(176, 245, 140, 1) 14%, rgba(220, 246, 149, 1) 36%, rgba(247, 234, 160, 1) 71%, rgba(248, 220, 174, 1) 100%);'>
+    <h4 style="text-align: center; padding-top: 8em; padding-bottom: 0;"><?= Yii::t('app', 'This certificate is granted for the completion of the course') ?>:</h4>
+    <h1 style="text-align: center; padding-top: 0;"><?= $title ?></h1>
 </body>
